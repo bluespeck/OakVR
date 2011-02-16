2c3a68fda3010bb50fc584c091e68fda1651e7a2
bluespeck <mstudorache@gmail.com>
2011-02-16 12:41:31 +0200

    Added basic project structure

---
+++ src/EngineTester/main.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+#include "../Engine/Engine.h"
+
+LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam );
+
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow )
+{
+	return 0;
+}
