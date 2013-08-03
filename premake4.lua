cee997276c8ee078dc6aac7ec1bbed6106f0dd99
bluespeck <mstudorache@gmail.com>
2013-08-03 10:31:30 +0300

    Changed default build config to linux64&fastdebug

--- premake4.lua
+++ premake4.lua
@@ -4,7 +4,7 @@ oakvrRoot = os.getcwd()
 dofile "premake-config/p4-config.lua"
 
 if _ACTION == "build" then
-	Build((_OPTIONS["buildplf"] or "linux32"), (_OPTIONS["buildcfg"] or "fastdebug"))
+	Build((_OPTIONS["buildplf"] or "linux64"), (_OPTIONS["buildcfg"] or "fastdebug"))
 else
 	dofile "src/p4-meta.lua"
 	dofile "src/p4-src.lua"
