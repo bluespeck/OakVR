291ced32b9459964320ce5781aa8231fa6de775a
bluespeck <mstudorache@gmail.com>
2014-07-04 09:12:48 +0300

    added tinyXML

---
+++ src/Core/CoreExternals/TinyXML2/__TinyXML2.lua
@@ -0,0 +1,11 @@
+Project "TinyXML2"
+	kind "StaticLib"
+	language "C++"
+
+	-- Version from May 20 2014
+	
+	files
+	{
+		"*.h",
+		"*.cpp",
+	}
--- src/Core/CoreExternals/__CoreExternals.lua
+++ src/Core/CoreExternals/__CoreExternals.lua
@@ -2,3 +2,4 @@
 	dofile("Zlib/__Zlib.lua")
 	dofile("Png/__Png.lua")
 	dofile("FreeType/__FreeType.lua")
+	dofile("TinyXML2/__TinyXML2.lua")
